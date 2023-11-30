bbf9a5b8e419d4f3e7c52cc5eb360da0aca9163f
kgajdamo <kinga.gajdamowicz@intel.com>
2023-11-30 17:12:14 +0000

    remove prints

--- torch_geometric/sampler/neighbor_sampler.py
+++ torch_geometric/sampler/neighbor_sampler.py
@@ -140,10 +140,7 @@ class NeighborSampler(BaseSampler):
                 data, device='cpu', share_memory=share_memory,
                 is_sorted=is_sorted, node_time_dict=self.node_time,
                 edge_time_dict=self.edge_time)
-            print("hetero colptr_dict:")
-            print(colptr_dict)
-            print("hetero row_dict:")
-            print(row_dict)
+
 
             self.row_dict = remap_keys(row_dict, self.to_rel_type)
             self.colptr_dict = remap_keys(colptr_dict, self.to_rel_type)
@@ -257,10 +254,7 @@ class NeighborSampler(BaseSampler):
                 self.row_dict = remap_keys(row_dict, self.to_rel_type)
                 self.colptr_dict = remap_keys(colptr_dict, self.to_rel_type)
 
-                print("dist_hetero colptr_dict:")
-                print(colptr_dict)
-                print("dist_hetero row_dict:")
-                print(row_dict)
+
 
         if (self.edge_time is not None
                 and not torch_geometric.typing.WITH_EDGE_TIME_NEIGHBOR_SAMPLE):
